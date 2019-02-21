a1e904f3a058b427dc703f6a7b507a4ba8e3050c
Lincoln Patrick <lincolnp23@gmail.com>
2019-02-20 21:36:18 -0300

    exercicio de numeros aleatorios e maximo numeral

---
+++ exercicio1.py
@@ -0,0 +1,15 @@
+import random as r
+
+v_lista = []
+i = 1
+
+while i <= 20:
+    v_lista.append(r.randrange(1, 100, 3))
+    i = i+1
+
+print(f'Essa foi a lista gerada aleatoriamente: {v_lista}')
+
+def maior_valor(v_lista):
+    return max(v_lista, key=int)
+
+print(f'Maior valor é: {maior_valor(v_lista)}')
